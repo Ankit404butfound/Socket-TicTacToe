fb56640f200b8a4c659b68d78f0a71dd0168c123
Ankit404butfound <ankitrajmahapatra@gmail.com>
2023-02-19 22:47:06 +0530

    V1

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, send
 
+import secrets
+
 
 app = Flask(__name__, static_folder="")
 app.config["SECRET"] = "secret"
@@ -16,25 +18,57 @@ def index():
     return render_template("index.html")
 
 
-@socket.on("join")
+@socket.on("createGame")
+def createGame():
+    sid = request.sid
+    gameId = secrets.token_hex(1)
+    gameDict[gameId] = {"players": [sid], sid: "X"}
+    returnJson = {"gameId": gameId, "player": "X"}
+    socket.emit("gameCreated", returnJson, room=sid)
+
+
+@socket.on("joinGame")
 def joinGame(data):
-    print("connected: ", request.sid)
-    player = ""
-    if gameDict:
-        player = "O"
+    gameId = data["gameId"]
+    sid = request.sid
+    if gameId in gameDict:
+        opponent = gameDict[gameId]["players"][0]
+        gameDict[gameId]["players"].append(sid)
+        gameDict[gameId][sid] = "O"
+        gameDict[gameId]["opponents"] = {sid: opponent, opponent: sid}
+        returnJson = {"gameId": gameId, "player": "O"}
+        socket.emit("opponentJoined", returnJson, room=opponent)
+        socket.emit("successJoin", returnJson, room=sid)
     else:
-        player = "X"
-    gameDict[request.sid] = player
-    print(gameDict)
-    socket.emit("joined", player, room=request.sid)
+        socket.emit("error", {"msg": "Invalid Game ID"}, room=sid)
+
+    # player = ""
+    # if gameDict:
+    #     player = "O"
+    #     socket.emit("opponentJoined")
+    # else:
+    #     player = "X"
+    # gameDict[request.sid] = player
+    # print(gameDict)
+    # socket.emit("joined", player, room=request.sid)
+
+
+@socket.on("replay")
+def replay(data):
+    gameId = data["gameId"]
+    players = gameDict[gameId]["players"]
+    for i in players:
+        returnJson = {"player": "X" if i == request.sid else "O"}
+        socket.emit("replay", returnJson, room=i)
+    
 
 
 @socket.on("move")
 def move(data):
-    for i in gameDict:
-        if i != request.sid:
-            opponent = i
-    socket.emit("opponentMove", data, room=opponent)
+    gameId = data["gameId"]
+    opponent = gameDict[gameId]["opponents"][request.sid]
+    returnJson = {"player": data["player"], "position": data["position"]}
+    socket.emit("opponentMove", returnJson, room=opponent)
 
 
 
@@ -43,6 +77,13 @@ def disconnect():
     print("disconnected: ", request.sid)
     del gameDict[request.sid]
     print(gameDict)
+
+
+
+def getOpponent(sid):
+    for i in gameDict:
+        if i != sid:
+            return i
 
 
 socket.run(app, debug=True, host="0.0.0.0", port=5000)
--- static/script.js
+++ static/script.js
@@ -1,37 +1,247 @@
 var thisPlayer = "";
-var socket;
+var socket = io.connect();
+var thisPlayersTurn = false;
+var opponentJoined = false;
+var gameId = "";
+var gameArray = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
 
-window.onload = function(e){
-    socket = io.connect();
-    socket.on("connect", () =>{
-        socket.emit("join", "hi");
+const btnJoin = document.getElementById("btnJoin");
+const btnCreate = document.getElementById("btnCreate");
+
+document.getElementById("gameArea").querySelectorAll("td").forEach(e => e.addEventListener("click", function(event){
+    console.log(this.textContent);
+    if (this.textContent != "" || !thisPlayersTurn || !opponentJoined){
+        return;
+    }
+    this.textContent = thisPlayer;
+    document.getElementById("msg").innerText = "Opponent's move";
+    gameArray[this.id[0]][this.id[1]] = thisPlayer;
+    thisPlayersTurn = false;
+    socket.emit("move", {
+        player: thisPlayer,
+        position: this.id,
+        gameId: gameId
     })
+    var winner = checkWinner();
+    if (winner != ""){
+        document.getElementById("msg").innerText = winner == "draw"? "Game draw!": `${winner} won the game`;
+        return;
+    }
+}))
+
+document.getElementById("btnReplay").addEventListener("click", function(){
+    socket.emit("replay", {gameId: gameId});
+    // thisPlayer = "X";
+});
+
+document.getElementById("gameArea").style.display = "none";
+document.getElementById("btnReplay").style.display = "none";
 
 
-    socket.on("joined", (data) =>{
-        console.log(data);
-        thisPlayer = data;
-        console.log(socket.id);
-    })
 
-    socket.on("disconnect", ()=>{
-        socket.emit("disconnect");
-    })
 
-    socket.on("opponentMove", (data) =>{
-        document.getElementById(data.position).textContent = data.player;
-    })
+btnCreate.addEventListener("click", function(){
+    socket.emit("createGame");
+});
 
-    document.querySelectorAll("td").forEach(e => e.addEventListener("click", function(event){
-       console.log(this.textContent);
-       if (this.textContent != ""){
-           return;
-       }
-       this.textContent = thisPlayer;
-       socket.emit("move", {
-            player: thisPlayer,
-            position: this.id
-       })
-    }))
+btnJoin.addEventListener("click", function(){
+    var gameId = prompt("Please enter Game ID");
+    socket.emit("joinGame", {gameId: gameId});
+    
+});
+
+
+// function addListenerToGameArea(){
+//     document.getElementById("gameArea").querySelectorAll("td").forEach(e => e.addEventListener("click", function(event){
+//         if (this.textContent != "" || !thisPlayersTurn || !opponentJoined){
+//             return;
+//         }
+//         this.textContent = thisPlayer;
+//         document.getElementById("msg").innerText = "Opponent's move";
+        
+//         thisPlayersTurn = false;
+//         socket.emit("move", {
+//             player: thisPlayer,
+//             position: this.id,
+//             gameId: gameId
+//         })
+//     }))
+// }
+
+
+function replay(data){
+        gameArray = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+        thisPlayer = data.player;
+        if (thisPlayer == "X"){
+            thisPlayersTurn = true;
+            document.getElementById("msg").innerText = "Your move";
+        }
+        else{
+            thisPlayersTurn = false;
+            document.getElementById("msg").innerText = "Opponent's move";
+        }
+        document.getElementById("gameArea").querySelectorAll("td").forEach(e => e.textContent = "");
 }
 
+
+
+function onGameCreated(data){
+    console.log("Game Created");
+    document.getElementById("userArea").style.display = "none";
+    document.getElementById("gameArea").style.display = "";
+    document.getElementById("btnReplay").style.display = "";
+    // addListenerToGameArea();
+    document.getElementById("msg").innerHTML = `Waiting for opponent to join<br><h6>Share Game ID: ${data.gameId}</h6>`;
+    gameId = data.gameId;
+    thisPlayer = data.player;
+    
+}
+
+
+function onOpponentJoined(data){
+    console.log("Opponent Joined");
+    document.getElementById("msg").innerHTML = "Opponent Joined, Your move";
+    thisPlayersTurn = true;
+    opponentJoined = true;
+}
+
+
+function onSuccessJoin(data){
+    console.log("Joined");
+    document.getElementById("gameArea").style.display = "";
+    document.getElementById("userArea").style.display = "none";
+    document.getElementById("btnReplay").style.display = "";
+    opponentJoined = true;
+    // addListenerToGameArea();
+    document.getElementById("msg").innerHTML = `Opponent's move`;
+    thisPlayer = data.player;
+    gameId = data.gameId;
+}
+
+
+function onOpponentMove(data){
+    position = data.position;
+    console.log(data);
+    document.getElementById(position).textContent = data.player;
+    gameArray[position[0]][position[1]] = data.player;
+    var winner = checkWinner();
+    if (winner != ""){
+        document.getElementById("msg").innerText = winner == "draw"? "Game draw!": `${winner} won the game`;
+        return;
+    }
+    thisPlayersTurn = true;
+    document.getElementById("msg").innerText = "Your move";
+
+}
+
+
+function checkWinner(){
+    for (var i = 0; i < 3; i++){
+        if (gameArray[i][0] == gameArray[i][1] && gameArray[i][1] == gameArray[i][2] && gameArray[i][0] != ""){
+            return gameArray[i][0];
+        }
+        if (gameArray[0][i] == gameArray[1][i] && gameArray[1][i] == gameArray[2][i] && gameArray[0][i] != ""){
+            return gameArray[0][i];
+        }
+    }
+
+    if (gameArray[0][0] == gameArray[1][1] && gameArray[1][1] == gameArray[2][2] && gameArray[0][0] != ""){
+        return gameArray[0][0];
+    }
+
+    if (gameArray[0][2] == gameArray[1][1] && gameArray[1][1] == gameArray[2][0] && gameArray[0][2] != ""){
+        return gameArray[0][2];
+    }
+
+    // Check for draw
+    var draw = true;
+    for (var i = 0; i < 3; i++){
+        for (var j = 0; j < 3; j++){
+            if (gameArray[i][j] == ""){
+                draw = false;
+                break
+            }
+        }
+    }
+    return draw ? "draw" : "";
+}
+
+
+socket.on("gameCreated", onGameCreated);
+socket.on("opponentJoined", onOpponentJoined);
+socket.on("successJoin", onSuccessJoin);
+socket.on("opponentMove", onOpponentMove);
+socket.on("replay", replay);
+socket.on("error", (data) => {
+    console.log(data);
+    alert(data.msg);
+    });
+
+
+
+
+
+    // socket.on("joined", (data) =>{
+    //     console.log(data);
+    //     thisPlayer = data;
+    //     if (thisPlayer == "X"){
+    //         thisPlayersTurn = true;
+    //     }
+    //     console.log(socket.id);
+    // })
+
+
+    // socket.on("opponentJoined", () =>{
+    //     console.log("Opponent Joined");
+    //     opponentJoined = true;
+    //     if (thisPlayer == "X"){
+    //         document.getElementById("waitmsg").innerText = "Opponent Joined, Your move";
+    //     }
+    //     else{
+    //         document.getElementById("waitmsg").innerText = "Opponent's move";
+    //     }
+    // });
+
+    
+
+    // socket.on("disconnect", ()=>{
+    //     socket.emit("disconnect");
+    // })
+
+    // socket.on("opponentMove", (data) =>{
+    //     document.getElementById(data.position).textContent = data.player;
+    //     thisPlayersTurn = true;
+    //     document.getElementById("waitmsg").innerText = "Your move";
+    // })
+
+    
+
+    // document.querySelectorAll("td").forEach(e => e.addEventListener("click", function(event){
+    //     console.log(this.textContent);
+    //     if (this.textContent != "" || !thisPlayersTurn || !opponentJoined){
+    //         return;
+    //     }
+    //     document.getElementById("waitmsg").innerText = "Opponent's move";
+    //     this.textContent = thisPlayer;
+    //     thisPlayersTurn = false;
+    //     socket.emit("move", {
+    //         player: thisPlayer,
+    //         position: this.id
+    //     })
+    // }))
+
+
+    // window.onload = function(e){
+    
+    //     socket.on("connect", () =>{
+            
+    //     })
+    // }
